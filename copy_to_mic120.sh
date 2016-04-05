d7042885d16db30474476f1dff303b5770973f9d
Gautier Vaillant <gautier.vaillant@gmail.com>
2016-04-05 16:30:08 +0200

    File created

---
+++ copy_to_mic120.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/sh
+
+scp -r -P 22000 . vaillantg@cluster.ig.fpms.ac.be:/home_nfs/vaillantg/grasp_scratch
---
+++ create_symbolic_links.sh
@@ -0,0 +1,6 @@
+ln -svf ./gcc/src/*.cpp ./novect/src
+ln -svf ./gcc/src/*.cpp ./vect/src
+
+
+ln -svf ./gcc/include/*.hpp ./vect/include
+ln -svf ./gcc/include/*.hpp ./vect/include
