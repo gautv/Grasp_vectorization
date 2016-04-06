93aad80a2dfbb14f1ab5b2d66cfb2dde4d76927e
Gautier Vaillant <gautier.vaillant@gmail.com>
2016-04-06 16:22:52 +0200

    Creation Branch Local Search

--- gcc/src/main.cpp
+++ gcc/src/main.cpp
@@ -1,13 +1,11 @@
 #include "../include/cost.h"
 
-
-integer processingTime0[SIZE][SIZE] __attribute__((aligned(64))) ;
+integer processingTime0[SIZE][SIZE] __attribute__((aligned(64)));
 integer processingTime1[SIZE][SIZE] __attribute__((aligned(64))) = {0};
-
 
 int main(int argc, char **argv) {
 
-  //Getting arguments ...
+  // Getting arguments ...
   int isVectorized = atoi(argv[1]);
   int numberIterations = atoi(argv[2]);
 
@@ -25,14 +23,52 @@ int main(int argc, char **argv) {
   if (isVectorized == 0) {
     for (int iteration = 0; iteration < numberIterations; iteration++)
       classic_cost(processingTime0);
-      } else {
+  } else {
     for (int iteration = 0; iteration < numberIterations; iteration++)
       new_cost(processingTime1);
   }
 
-  
-  std::cout << classic_cost(processingTime0) << std::endl;
-  std::cout << new_cost(processingTime1) << std::endl;
+  //--- GENERATE INITIAL SOLUTION ---
+  integer permutation[J];
 
-  return 0;
+  for (int j = 0, j < J; j++)
+    permutation[j] = j;
+
+  //--- LOCAL SEARCH ---
+  {
+    integer min_cost = 99999;
+    integer min_j1 = 1;
+    integer min_j2 = -1;
+    bool has_found_better_minimum = true;
+
+    while (has_found_better_minimum) {
+
+      integer before_cost = min_cost;
+
+      // Iteration through each elements (Permutation in O(N^2))
+      for (integer i1 = 0; i1 < J; i1++)
+        for (integer i2 = 0; i2 < J; i2++) // OPTIMIZATION : i2 < i1 ????
+        {
+          // Generation Neighbour
+          std::swap(permutation[i1], permutation[i2]);
+
+          // Computation of the cost
+          integer current_cost = classic_cost(processingTime0);
+
+          // Go back to the Original Permutation
+          std::swap(permutation[i1], permutation[i2]);
+
+          if (current_cost < min_cost) {
+            min_cost = currrent_cost;
+            min_i1 = i1;
+            min_i2 = i2;
+          }
+        }
+
+      has_found_better_minimum = (min_cost < before_cost)
+    }
+  }
+}
+
+return 0;
 }
