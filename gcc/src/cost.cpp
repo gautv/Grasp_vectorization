82b3a9ceccc0b60fa19e1e717cded697679f0c47
Gautier Vaillant <gautier.vaillant@gmail.com>
2016-04-07 14:14:27 +0200

    Permutations added

--- gcc/src/cost.cpp
+++ gcc/src/cost.cpp
@@ -1,30 +1,36 @@
 #include "../include/cost.h"
 
-integer memoryChunk1[64*2] __attribute__((aligned(64))) ;
-integer memoryChunk2[64*2] __attribute__((aligned(64)));
+integer memoryChunk1[64 * 2] __attribute__((aligned(64)));
+integer memoryChunk2[64 * 2] __attribute__((aligned(64)));
 
-integer classic_cost(integer processing_times0[SIZE][SIZE]) {
+integer classic_cost(integer permutation[SIZE],
+                     integer processing_times0[SIZE][SIZE]) {
 
   integer time[M] = {0};
 
-  for (integer j = 0; j < J; j++)
+  for (integer j = 0; j < J; j++) {
+    integer job = permutation[j];
     for (integer m = 0; m < M; m++) {
-      time[m] = std::max(time[m], time[m - 1]) + processing_times0[m][j];
+      time[m] = std::max(time[m], time[m - 1]) + processing_times0[m][job];
     }
+  }
   return time[M - 1];
 }
 
-integer new_cost(integer processing_time1[SIZE][SIZE]) {
+integer new_cost(integer permutation[SIZE],
+                 integer processing_time1[SIZE][SIZE]) {
 
-  integer* oldTime = memoryChunk1;
-  integer* newTime = memoryChunk2;
+  integer *oldTime = memoryChunk1;
+  integer *newTime = memoryChunk2;
 
-  for (integer m = 0; m < M1; m++)
-    oldTime[m] = processing_time1[m][0 + M - m];
+  for (integer m = 0; m < M1; m++) {
+    int job = permutation[0 + M - m];
+    oldTime[m] = processing_time1[m][job];
+  }
 
   for (int j = 1; j < J1; j++) {
-      for (int m = 1; m < M1; m++) {
-      integer job = j + M - m;
+    for (int m = 1; m < M1; m++) {
+      integer job = permutation[j + M - m];
       newTime[m] =
           std::max(oldTime[m], oldTime[m - 1]) + processing_time1[m][job];
       //
--- gcc/src/main.cpp
+++ gcc/src/main.cpp
@@ -1,17 +1,31 @@
 #include "../include/cost.h"
-
-
-integer processingTime0[SIZE][SIZE] __attribute__((aligned(64))) ;
-integer processingTime1[SIZE][SIZE] __attribute__((aligned(64))) = {0};
-
-
+#include <cstdio>
 int main(int argc, char **argv) {
 
-  //Getting arguments ...
+  // Getting arguments ...
   int isVectorized = atoi(argv[1]);
   int numberIterations = atoi(argv[2]);
 
-  // Initialisation Processing Times
+  // Intialisation permutation
+
+  integer permutation0[SIZE] __attribute__((aligned(64)));
+  integer permutation1[SIZE] __attribute__((aligned(64)));
+
+  for (int j = 0; j < J; j++)
+    permutation0[j] = j;
+
+  for (int j = 0; j < J1; ++j) {
+    if (j < (M - 1) || j >= J + (M - 1))
+      permutation1[j] = 0;
+    else
+      permutation1[j] = permutation0[j - M + 1] + (M - 1);
+  }
+
+   // Initialisation Processing Times
+
+  integer processingTime0[SIZE][SIZE] __attribute__((aligned(64)));
+  integer processingTime1[SIZE][SIZE] __attribute__((aligned(64))) = {0};
+
   for (integer j = 0; j < J; j++)
     for (integer m = 0; m < M; m++)
       processingTime0[m][j] = m + j;
@@ -24,15 +38,16 @@ int main(int argc, char **argv) {
 
   if (isVectorized == 0) {
     for (int iteration = 0; iteration < numberIterations; iteration++)
-      classic_cost(processingTime0);
-      } else {
+      classic_cost(permutation0, processingTime0);
+  } else {
     for (int iteration = 0; iteration < numberIterations; iteration++)
-      new_cost(processingTime1);
+      new_cost(permutation1, processingTime1);
   }
 
-  
-  std::cout << classic_cost(processingTime0) << std::endl;
-  std::cout << new_cost(processingTime1) << std::endl;
+  printf("\n\n --- Results ---\n");
+
+  std::cout << classic_cost(permutation0, processingTime0) << std::endl;
+  std::cout << new_cost(permutation1, processingTime1) << std::endl;
 
   return 0;
 }
